d00a71c560cedd2cc842bb65d4ea8ac4ef1e3693
huseyin corakli <huseyincorakli@icloud.com>
2023-08-27 10:37:32 +0300

    Modal

--- src/admin/pages/Product.tsx
+++ src/admin/pages/Product.tsx
@@ -1,19 +1,9 @@
-import { useState } from 'react';
 import ProductList from '../components/ProductList'
 import 'react-toastify/dist/ReactToastify.css'
-import { ProductAddModal } from '../../modal/Product/ProductDeleteModal';
-import { Button } from '@mui/material';
-import { ProductDeleteModal } from '../../modal/Product/DeleteProductModal';
 
 const Product = () => {
-  
-  
-
- 
- 
   return (
     <div>
-      
       <div className='bg-white mt-3 p-2 rounded-lg shadow-md mx-10'>
         <ProductList />
       </div>
