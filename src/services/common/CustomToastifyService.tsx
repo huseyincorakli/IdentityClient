0af5910a0f7b4abd53548c66edee54ceee3bd880
huseyin corakli <huseyincorakli@icloud.com>
2023-08-27 08:29:16 +0300

    Custom Toastify Service :)

---
+++ src/services/common/CustomToastifyService.tsx
@@ -0,0 +1,50 @@
+import { toast, ToastContainer } from 'react-toastify'
+
+export const ToastifyService = {
+  notfy (
+    message: string,
+    type: MessageType,
+    options:Partial<CTS_Options>
+  ) {
+    const isHide = options.hide == 1 ? true : false
+    toast[type](message, {
+      autoClose: options.timeoutDuration,
+      hideProgressBar: isHide,
+      position:options.position
+    })
+  }
+}
+
+export const CustomToastifyContainer = () => {
+  return <ToastContainer />
+}
+export enum Position{
+ TopLeft= "top-left",
+ TopRight="top-right",
+ TopCenter="top-center",
+ BottomLeft="bottom-left",
+ BottomRight="bottom-right",
+ BottomCenter="bottom-center"
+}
+export enum MessageType {
+  Success = 'success',
+  Warning = 'warning',
+  Info = 'info'
+}
+
+export enum TimeoutDuration {
+  Fast = 2000,
+  Slow = 5000,
+  Moderate = 3000
+}
+
+export enum Hide {
+  True = 1,
+  False = 0
+}
+
+export class CTS_Options{
+  timeoutDuration?: TimeoutDuration=TimeoutDuration.Fast;
+  hide?: Hide=Hide.False;
+  position?:Position=Position.TopRight;
+}
